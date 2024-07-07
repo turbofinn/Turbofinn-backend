3f89414b7241d35e1e670c764d69d9ab07ffd1f7
gaurav singh <gaurav.singh@hummingwave.com>
2024-07-08 00:57:07 +0530

    Send otp

--- src/main/java/org/turbofinn/util/Constants.java
+++ src/main/java/org/turbofinn/util/Constants.java
@@ -3,6 +3,16 @@ package org.turbofinn.util;
 public class Constants {
     public static final boolean IS_PROD = false;
     public static final boolean IS_PROD_TESTING = false;
-    public static final boolean USE_LOCAL_AWS_CREDENTIALS = false;
+    public static final boolean USE_LOCAL_AWS_CREDENTIALS = true;
     public static final String LOCAL_AWS_CREDENTIALS_PROFILE = "turbofinn";
+
+
+
+    public static final int SUCCESS_RESPONSE_CODE = 1001;
+    public static final int INVALID_INPUTS_RESPONSE_CODE = 9999;
+    public static final String SUCCESS_RESPONSE_MESSAGE = "Success";
+    public static final String INVALID_INPUTS_RESPONSE_MESSAGE = "Invalid inputs";
+
+
+
 }
