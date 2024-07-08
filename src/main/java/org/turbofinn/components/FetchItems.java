dc3003db58310c37500d7589cf7d03d86b905f9f
saurabhmaurya1 <saurabhmaurya155@gmail.com>
2024-07-09 02:07:42 +0530

    Included by category and tag methods

--- src/main/java/org/turbofinn/components/FetchItems.java
+++ src/main/java/org/turbofinn/components/FetchItems.java
@@ -19,16 +19,25 @@ public class FetchItems implements RequestHandler<FetchItems.FetchItemsInput,Fet
     public static void main(String[] args) {
         FetchItemsInput input = new FetchItemsInput();
         input.setRestaurantId("5cef7136-ff4e-418a-ba00-5ef18d67bd43");
+        input.setCategory("noodels");
+        input.setTag("dineIn");
         System.out.println(new Gson().toJson(new FetchItems().handleRequest(input,null)));
     }
     @Override
     public FetchItems.FetchItemsOutput handleRequest(FetchItems.FetchItemsInput fetchItemsInput, Context context) {
-        if(fetchItemsInput==null || fetchItemsInput.restaurantId==null){
+        if(fetchItemsInput==null || fetchItemsInput.restaurantId==null || fetchItemsInput.tag==null){
             return new FetchItems.FetchItemsOutput(new Response(Constants.INVALID_INPUTS_RESPONSE_CODE,Constants.INVALID_INPUTS_RESPONSE_MESSAGE),null);
 
         }
+        List<DB_Items> items = null;
+        items= DB_Items.fetchItemsByRestaurantIDAndTag(fetchItemsInput.getRestaurantId(),fetchItemsInput.tag);
+        if(fetchItemsInput.getCategory()!=null){
+          items = items.stream().filter(x->x.getCategory().equalsIgnoreCase(fetchItemsInput.category)).toList();
+        }
+        if(fetchItemsInput.getFlag()!=null){
+            items =items.stream().filter(x->x.getCategory().equalsIgnoreCase(fetchItemsInput.flag)).toList();
+        }
 
-        List<DB_Items> items= DB_Items.fetchItemsByRestaurantID(fetchItemsInput.getRestaurantId());
         if (items.isEmpty()){
             return new FetchItems.FetchItemsOutput(new Response(Constants.INVALID_INPUTS_RESPONSE_CODE,Constants.INVALID_INPUTS_RESPONSE_MESSAGE),null);
         }
@@ -45,6 +54,9 @@ public class FetchItems implements RequestHandler<FetchItems.FetchItemsInput,Fet
     @Getter@Setter@NoArgsConstructor@AllArgsConstructor
     public static class FetchItemsInput {
         public String restaurantId;
+        public String category;
+        public String tag;
+        public String flag;
     }
     @Getter@Setter@AllArgsConstructor@NoArgsConstructor
     public static  class Response{
