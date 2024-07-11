7517a5b82d53ef434f415a073923f17b16cc2c3b
saurabhmaurya1 <saurabhmaurya155@gmail.com>
2024-07-11 20:39:30 +0530

    Added Constants

--- src/main/java/org/turbofinn/util/Constants.java
+++ src/main/java/org/turbofinn/util/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
 
     public static final int SUCCESS_RESPONSE_CODE = 1001;
     public static final int INVALID_INPUTS_RESPONSE_CODE = 9999;
+    public static final int GENERIC_RESPONSE_CODE = 5001;
     public static final String SUCCESS_RESPONSE_MESSAGE = "Success";
     public static final String INVALID_INPUTS_RESPONSE_MESSAGE = "Invalid inputs";
 
