aec472cad7293f7d45f43c60623fc01de20a2e7c
saurabhmaurya1 <saurabhmaurya155@gmail.com>
2025-02-06 22:49:12 +0530

    updated rds password

--- src/main/java/org/turbofinn/util/Constants.java
+++ src/main/java/org/turbofinn/util/Constants.java
@@ -3,7 +3,7 @@ package org.turbofinn.util;
 public class Constants {
     public static final boolean IS_PROD = false;
     public static final boolean IS_PROD_TESTING = false;
-    public static final boolean USE_LOCAL_AWS_CREDENTIALS = true;
+    public static final boolean USE_LOCAL_AWS_CREDENTIALS = false;
     public static final String LOCAL_AWS_CREDENTIALS_PROFILE = "turbofinn";
 
     public static final int SUCCESS_RESPONSE_CODE = 1001;
@@ -17,7 +17,7 @@ public class Constants {
 
     public static final String RDS_URL = "jdbc:mysql://turbofinn-dev.cjmo2k0q4pt6.ap-south-1.rds.amazonaws.com:3306/Turbofinn";
     public static final String RDS_USERNAME = "root";
-    public static final String RDS_PASSWORD = "turbofinn123";
+    public static final String RDS_PASSWORD = "turbofinn111";
 
 
 
